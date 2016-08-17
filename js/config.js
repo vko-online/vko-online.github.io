0f61212c2a4f9a64b665425b2a670449dbb00e61
Medet <m.diddy_11@mail.ru>
2016-08-18 02:02:36 +0600

    initial :rocket:

--- js/config.js
+++ js/config.js
@@ -3,20 +3,20 @@ $(function() {
   CMS.init({
 
     // Name of your site or location of logo file, relative to root directory (img/logo.png)
-    siteName: 'My Site',
+    siteName: 'Personal notebook',
 
     // Tagline for your site
-    siteTagline: 'Your site tagline',
+    siteTagline: 'developer almaty',
 
     // Email address
-    siteEmail: 'your_email@example.com',
+    siteEmail: 'm.tleukabiluly@gmail.com',
 
     // Name
-    siteAuthor: 'Your Name',
+    siteAuthor: 'Medet Tleukabiluly',
 
     // Navigation items
     siteNavItems: [
-      { name: 'Github', href: 'https://github.com/yourname', newWindow: false},
+      { name: 'Github', href: 'https://github.com/vko-online', newWindow: false},
       { name: 'About'}
     ],
 
@@ -45,7 +45,7 @@ $(function() {
     fadeSpeed: 300,
 
     // Site footer text
-    footerText: '&copy; ' + new Date().getFullYear() + ' All Rights Reserved.',
+    footerText: '&copy; ' + new Date().getFullYear() + ' All Rights... you know ;)',
 
     // Mode 'Github' for Github Pages, 'Server' for Self Hosted. Defaults
     // to Github
@@ -53,14 +53,14 @@ $(function() {
 
      // If Github mode is set, your Github username and repo name.
     githubUserSettings: {
-      username: 'yourusername',
-      repo: 'yourrepo'
+      username: 'vko-online',
+      repo: 'vko-online.github.io'
     },
 
     // If Github mode is set, choose which Github branch to get files from.
     // Defaults to Github pages branch (gh-pages)
     githubSettings: {
-      branch: 'gh-pages',
+      branch: 'master',
       host: 'https://api.github.com'
     }
 
